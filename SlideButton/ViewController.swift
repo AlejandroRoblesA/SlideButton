6d20537e0a878d864bce9d8b57f55e47b533598a
Alejandro <alejandro.rob.alt@gmail.com>
2019-08-18 14:55:29 -0500

    code replaced by function templeteGestureRecognizer

--- SlideButton/ViewController.swift
+++ SlideButton/ViewController.swift
@@ -124,13 +124,6 @@ class ViewController: UIViewController {
         upButtonView.isUserInteractionEnabled = true
         upButtonView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleUpPanMoveView(recognizer:))))
         
-        //  Convert to variable:
-        //  72. rightButtonView
-        //  72. x: areaSlideToRightView.layer.bounds.size.width-50 and y:0
-        //  77. in: self.areaSlideToRightView).x / (CGFloat(self.areaSlideToRightView.layer.bounds.size.width)
-        //  87. self.rightButtonView.transform = CGAffineTransform.identity
-        //
-        
     }
     
     func templeteGestureRecognizer(recognizer: UIPanGestureRecognizer, buttonView: UIView, translationX: CGFloat, translationY: CGFloat, transtationIn: CGFloat){
@@ -168,127 +161,32 @@ class ViewController: UIViewController {
         let tranlationIn = recognizer.translation(in: self.areaSlideToRightView).x / (CGFloat(self.areaSlideToRightView.layer.bounds.size.width))
         
         templeteGestureRecognizer(recognizer: recognizer, buttonView: self.rightButtonView, translationX: x, translationY: 0, transtationIn: tranlationIn)
-        
-//        switch recognizer.state {
-//        case .began:
-//            animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeOut, animations: {
-//                self.rightButtonView.transform = CGAffineTransform(translationX: CGFloat(self.areaSlideToRightView.layer.bounds.size.width-50) , y: 0)
-//            })
-//            animator.startAnimation()
-//            animator.pauseAnimation()
-//        case .changed:
-//            animator.fractionComplete = recognizer.translation(in: self.areaSlideToRightView).x / (CGFloat(self.areaSlideToRightView.layer.bounds.size.width))
-//        case .ended:
-//            if animator.fractionComplete > 0.80 {
-//                animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
-//                let s = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "test") as! Test
-//                navigationController?.pushViewController(s, animated: true)
-//            }
-//            else {
-//                animator.stopAnimation(true)
-//                UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveLinear, animations: {
-//                    self.rightButtonView.transform = CGAffineTransform.identity
-//                }, completion: { (completed) in
-//
-//                })
-//            }
-//        default:
-//            ()
-//        }
     }
 
     @objc func handleDownPanMoveView(recognizer: UIPanGestureRecognizer) {
         
-        switch recognizer.state {
-        case .began:
-            animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeOut, animations: {
-                self.downButtonView.transform = CGAffineTransform(translationX: 0 , y: CGFloat(self.areaSlideToDownView.layer.bounds.size.height-50))
-            })
-            animator.startAnimation()
-            animator.pauseAnimation()
-        case .changed:
-            animator.fractionComplete = recognizer.translation(in: self.areaSlideToDownView).y / (CGFloat(self.areaSlideToDownView.layer.bounds.size.height))
-        case .ended:
-            if animator.fractionComplete > 0.80 {
-                animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
-                let s = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "test") as! Test
-                self.present(s, animated: true)
-            }
-            else {
-                animator.stopAnimation(true)
-                UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveLinear, animations: {
-                    self.downButtonView.transform = CGAffineTransform.identity
-                }, completion: { (completed) in
-                    
-                })
-            }
-            
-            
-        default:
-            ()
-        }
+        let y = CGFloat(self.areaSlideToDownView.layer.bounds.size.height-50)
+        let translationIn = recognizer.translation(in: self.areaSlideToDownView).y / (CGFloat(self.areaSlideToDownView.layer.bounds.size.height))
+        
+        templeteGestureRecognizer(recognizer: recognizer, buttonView: downButtonView, translationX: 0, translationY: y, transtationIn: translationIn)
     }
     
     @objc func handleLeftPanMoveView(recognizer: UIPanGestureRecognizer) {
         
-        switch recognizer.state {
-        case .began:
-            animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeOut, animations: {
-                self.leftButtonView.transform = CGAffineTransform(translationX: -CGFloat(self.areaSlideToLeftView.layer.bounds.size.width-50) , y: 0)
-            })
-            animator.startAnimation()
-            animator.pauseAnimation()
-        case .changed:
-            animator.fractionComplete = recognizer.translation(in: self.areaSlideToLeftView).x / (-CGFloat(self.areaSlideToLeftView.layer.bounds.size.width))
-        case .ended:
-            if animator.fractionComplete > 0.80 {
-                animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
-                let s = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "test") as! Test
-                self.present(s, animated: true)
-            }
-            else {
-                animator.stopAnimation(true)
-                UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveLinear, animations: {
-                    self.leftButtonView.transform = CGAffineTransform.identity
-                }, completion: { (completed) in
-                    
-                })
-            }
-            
-            
-        default:
-            ()
-        }
+        let x = -CGFloat(self.areaSlideToLeftView.layer.bounds.size.width-50)
+        let translationIn = recognizer.translation(in: self.areaSlideToLeftView).x / (-CGFloat(self.areaSlideToLeftView.layer.bounds.size.width))
+        
+        templeteGestureRecognizer(recognizer: recognizer, buttonView: leftButtonView, translationX: x, translationY: 0, transtationIn: translationIn)
+
     }
     
     @objc func handleUpPanMoveView(recognizer: UIPanGestureRecognizer) {
         
-        switch recognizer.state {
-        case .began:
-            animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeOut, animations: {
-                self.upButtonView.transform = CGAffineTransform(translationX: 0 , y: -CGFloat(self.areaSlideToUpView.layer.bounds.size.height-50))
-            })
-            animator.startAnimation()
-            animator.pauseAnimation()
-        case .changed:
-            animator.fractionComplete = recognizer.translation(in: self.areaSlideToUpView).y / (-CGFloat(self.areaSlideToUpView.layer.bounds.size.height))
-        case .ended:
-            if animator.fractionComplete > 0.80 {
-                animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
-                let s = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "test") as! Test
-                self.present(s, animated: true)
-            }
-            else {
-                animator.stopAnimation(true)
-                UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveLinear, animations: {
-                    self.upButtonView.transform = CGAffineTransform.identity
-                }, completion: { (completed) in
-                    
-                })
-            }
-        default:
-            ()
-        }
+        let y = -CGFloat(self.areaSlideToUpView.layer.bounds.size.height-50)
+        let translationIn = recognizer.translation(in: self.areaSlideToUpView).y / (-CGFloat(self.areaSlideToUpView.layer.bounds.size.height))
+        
+        templeteGestureRecognizer(recognizer: recognizer, buttonView: upButtonView, translationX: 0, translationY: y, transtationIn: translationIn)
+        
     }
     
     @IBAction func rightActionButton(_ sender: UIButton) {
@@ -305,7 +203,6 @@ class ViewController: UIViewController {
     }
     
 }
-
 
 extension UIView{
     
